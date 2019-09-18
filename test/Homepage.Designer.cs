a71d14391f3edf523d5de5bca4145b4a0dbfc239
liuxin_small <2338846471@qq.com>
2019-09-18 17:25:26 +0800

    first

---
+++ test/Homepage.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Gige_Vidion_Camera
+{
+    partial class Homepage
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_opencamera = new System.Windows.Forms.Button();
+            this.button_grab = new System.Windows.Forms.Button();
+            this.button_freeze = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripLabel();
+            this.StatusLabelInfo = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.PixelLabel = new System.Windows.Forms.ToolStripLabel();
+            this.PixelDataValue = new System.Windows.Forms.ToolStripLabel();
+            this.button_closecamera = new System.Windows.Forms.Button();
+            this.button_View = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button_opencamera
+            // 
+            this.button_opencamera.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_opencamera.Location = new System.Drawing.Point(12, 62);
+            this.button_opencamera.Name = "button_opencamera";
+            this.button_opencamera.Size = new System.Drawing.Size(169, 76);
+            this.button_opencamera.TabIndex = 0;
+            this.button_opencamera.Text = "打开相机";
+            this.button_opencamera.UseVisualStyleBackColor = true;
+            this.button_opencamera.Click += new System.EventHandler(this.button_opencamera_Click);
+            // 
+            // button_grab
+            // 
+            this.button_grab.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_grab.Location = new System.Drawing.Point(12, 186);
+            this.button_grab.Name = "button_grab";
+            this.button_grab.Size = new System.Drawing.Size(169, 76);
+            this.button_grab.TabIndex = 1;
+            this.button_grab.Text = "采集图像";
+            this.button_grab.UseVisualStyleBackColor = true;
+            this.button_grab.Click += new System.EventHandler(this.button_grab_Click);
+            // 
+            // button_freeze
+            // 
+            this.button_freeze.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_freeze.Location = new System.Drawing.Point(12, 320);
+            this.button_freeze.Name = "button_freeze";
+            this.button_freeze.Size = new System.Drawing.Size(169, 76);
+            this.button_freeze.TabIndex = 2;
+            this.button_freeze.Text = "停止采集";
+            this.button_freeze.UseVisualStyleBackColor = true;
+            this.button_freeze.Click += new System.EventHandler(this.button_freeze_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel,
+            this.StatusLabelInfo,
+            this.toolStripSeparator1,
+            this.PixelLabel,
+            this.PixelDataValue});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1205, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(58, 22);
+            this.StatusLabel.Text = "Status:";
+            // 
+            // StatusLabelInfo
+            // 
+            this.StatusLabelInfo.Name = "StatusLabelInfo";
+            this.StatusLabelInfo.Size = new System.Drawing.Size(66, 22);
+            this.StatusLabelInfo.Text = "nothing";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // PixelLabel
+            // 
+            this.PixelLabel.Name = "PixelLabel";
+            this.PixelLabel.Size = new System.Drawing.Size(47, 22);
+            this.PixelLabel.Text = "Pixel:";
+            // 
+            // PixelDataValue
+            // 
+            this.PixelDataValue.Name = "PixelDataValue";
+            this.PixelDataValue.Size = new System.Drawing.Size(126, 22);
+            this.PixelDataValue.Text = "Data not avaible";
+            // 
+            // button_closecamera
+            // 
+            this.button_closecamera.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_closecamera.Location = new System.Drawing.Point(12, 449);
+            this.button_closecamera.Name = "button_closecamera";
+            this.button_closecamera.Size = new System.Drawing.Size(169, 76);
+            this.button_closecamera.TabIndex = 4;
+            this.button_closecamera.Text = "关闭相机";
+            this.button_closecamera.UseVisualStyleBackColor = true;
+            this.button_closecamera.Click += new System.EventHandler(this.button_closecamera_Click);
+            // 
+            // button_View
+            // 
+            this.button_View.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_View.Location = new System.Drawing.Point(12, 580);
+            this.button_View.Name = "button_View";
+            this.button_View.Size = new System.Drawing.Size(169, 76);
+            this.button_View.TabIndex = 5;
+            this.button_View.Text = "View";
+            this.button_View.UseVisualStyleBackColor = true;
+            this.button_View.Click += new System.EventHandler(this.button_View_Click);
+            // 
+            // Homepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1205, 718);
+            this.Controls.Add(this.button_View);
+            this.Controls.Add(this.button_closecamera);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button_freeze);
+            this.Controls.Add(this.button_grab);
+            this.Controls.Add(this.button_opencamera);
+            this.Name = "Homepage";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_opencamera;
+        private System.Windows.Forms.Button button_grab;
+        private System.Windows.Forms.Button button_freeze;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel StatusLabel;
+        private System.Windows.Forms.ToolStripLabel StatusLabelInfo;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel PixelLabel;
+        private System.Windows.Forms.ToolStripLabel PixelDataValue;
+        private System.Windows.Forms.Button button_closecamera;
+        private System.Windows.Forms.Button button_View;
+    }
+}
+
---
+++ test/Homepage.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DALSA.SaperaLT.SapClassBasic;
+using DALSA.SaperaLT.SapClassGui;
+
+namespace Gige_Vidion_Camera
+{
+    public partial class Homepage : Form
+    {
+        public Homepage()
+        {
+            InitializeComponent();
+            m_AcqDevice = null;
+            m_Buffers = null;
+            m_Xfer = null;
+            m_View = null;
+                // Note:
+                //  The code to initialize m_ImageBox was originally in the InitializeComponent function
+                //  called above. However, it has been moved to the dialog constructor as a workaround
+                //  to a Visual Studio Designer error when loading the DALSA.SaperaLT.SapClassBasic
+                //  assembly under 64-bit Windows.
+                //  As a consequence, it is not possible to adjust the m_ImageBox properties
+                //  automatically using the Designer anymore, this has to be done manually.
+                // 
+                this.m_ImageBox = new DALSA.SaperaLT.SapClassGui.ImageBox();
+                this.m_ImageBox.Location = new System.Drawing.Point(241, 4);
+                this.m_ImageBox.Name = "m_ImageBox";
+                this.m_ImageBox.PixelValueDisplay = this.PixelDataValue;
+                this.m_ImageBox.Size = new System.Drawing.Size(386, 359);
+                this.m_ImageBox.SliderEnable = true;
+                this.m_ImageBox.SliderMaximum = 10;
+                this.m_ImageBox.SliderMinimum = 0;
+                this.m_ImageBox.SliderValue = 0;
+                this.m_ImageBox.SliderVisible = false;
+                this.m_ImageBox.TabIndex = 12;
+                this.m_ImageBox.TrackerEnable = false;
+                this.m_ImageBox.View = null;
+                this.Controls.Add(this.m_ImageBox);
+
+                
+        }
+        private SapAcqDevice m_AcqDevice;
+        private SapBuffer m_Buffers;
+        private SapAcqDeviceToBuf m_Xfer;
+        private SapView m_View;
+        private SapLocation m_ServerLocation;
+        private string m_ConfigFileName;
+        private ImageBox m_ImageBox;
+        private delegate void DisplayFrameAcquired(int number, bool trash);
+        static void xfer_XferNotify(object sender, SapXferNotifyEventArgs argsNotify)
+        {
+            Homepage GigeDlg = argsNotify.Context as Homepage;
+            // If grabbing in trash buffer, do not display the image, update the
+            // appropriate number of frames on the status bar instead
+            if (argsNotify.Trash)
+                GigeDlg.Invoke(new DisplayFrameAcquired(GigeDlg.ShowFrameNumber), argsNotify.EventCount, true);
+            else
+            {
+                GigeDlg.Invoke(new DisplayFrameAcquired(GigeDlg.ShowFrameNumber), argsNotify.EventCount, false);
+                GigeDlg.m_View.Show();
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+     /// <summary>
+     /// 初始化设备
+     /// </summary>
+     /// <param name="acConfigDlg"></param>
+     /// <param name="Restore"></param>
+     /// <returns></returns>
+        public bool CreateNewObjects(AcqConfigDlg acConfigDlg, bool Restore)
+        {
+            if (!Restore)
+            {
+                m_ServerLocation = acConfigDlg.ServerLocation;
+                m_ConfigFileName = acConfigDlg.ConfigFile;
+            }
+            m_AcqDevice = new SapAcqDevice(m_ServerLocation, m_ConfigFileName);
+            if (SapBuffer.IsBufferTypeSupported(m_ServerLocation, SapBuffer.MemoryType.ScatterGather))
+                m_Buffers = new SapBufferWithTrash(2, m_AcqDevice, SapBuffer.MemoryType.ScatterGather);
+            else
+                m_Buffers = new SapBufferWithTrash(2, m_AcqDevice, SapBuffer.MemoryType.ScatterGatherPhysical);
+            m_Xfer = new SapAcqDeviceToBuf(m_AcqDevice, m_Buffers);
+            m_View = new SapView(m_Buffers);
+            m_ImageBox.View = m_View;
+
+            m_Xfer.Pairs[0].EventType = SapXferPair.XferEventType.EndOfFrame;
+            m_Xfer.XferNotify += new SapXferNotifyHandler(xfer_XferNotify);
+            m_Xfer.XferNotifyContext = this;
+            StatusLabelInfo.Text = "Online... Waiting grabbed images";
+
+            if (!CreateObjects())
+            {
+                DisposeObjects();
+                return false;
+            }
+
+            // Resize ImagBox to take into account the size of created sapview
+            m_ImageBox.OnSize();
+            UpdateControls();
+            return true;
+        }
+        /// <summary>
+        /// 找寻框架
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="trash"></param>
+        private void ShowFrameNumber(int number, bool trash)
+        {
+            String str;
+            if (trash)
+            {
+                str = String.Format("Frames acquired in trash buffer: {0}", number);
+                Console.WriteLine(str);
+            }
+            else
+            {
+                str = String.Format("Frames acquired :{0}", number);
+                this.StatusLabelInfo.Text = str;
+            }
+        }
+        /// <summary>
+        /// 注册回调
+        /// </summary>
+        /// <returns></returns>
+        private bool CreateObjects()
+        {
+            // Create acquisition object
+            if (m_AcqDevice != null && !m_AcqDevice.Initialized)
+            {
+                if (m_AcqDevice.Create() == false)
+                {
+                    DestroyObjects();
+                    return false;
+                }
+            }
+            // Create buffer object
+            if (m_Buffers != null && !m_Buffers.Initialized)
+            {
+                if (m_Buffers.Create() == false)
+                {
+                    DestroyObjects();
+                    return false;
+                }
+                m_Buffers.Clear();
+            }
+            // Create view object
+            if (m_View != null && !m_View.Initialized)
+            {
+                if (m_View.Create() == false)
+                {
+                    DestroyObjects();
+                    return false;
+                }
+            }
+
+            if (m_Xfer != null && m_Xfer.Pairs[0] != null)
+            {
+                m_Xfer.Pairs[0].Cycle = SapXferPair.CycleMode.NextWithTrash;
+                if (m_Xfer.Pairs[0].Cycle != SapXferPair.CycleMode.NextWithTrash)
+                {
+                    DestroyObjects();
+                    return false;
+                }
+            }
+
+            // Create Xfer object
+            if (m_Xfer != null && !m_Xfer.Initialized)
+            {
+                if (m_Xfer.Create() == false)
+                {
+                    DestroyObjects();
+                    return false;
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// 清理内存
+        /// </summary>
+        private void DestroyObjects()
+        {
+            if (m_Xfer != null && m_Xfer.Initialized)
+                m_Xfer.Destroy();
+            if (m_View != null && m_View.Initialized)
+                m_View.Destroy();
+            if (m_Buffers != null && m_Buffers.Initialized)
+                m_Buffers.Destroy();
+            if (m_AcqDevice != null && m_AcqDevice.Initialized)
+                m_AcqDevice.Destroy();
+        }
+        /// <summary>
+        /// 释放句柄
+        /// </summary>
+        private void DisposeObjects()
+        {
+            if (m_Xfer != null)
+            { m_Xfer.Dispose(); m_Xfer = null; }
+            if (m_View != null)
+            { m_View.Dispose(); m_View = null; m_ImageBox.View = null; }
+            if (m_Buffers != null)
+            { m_Buffers.Dispose(); m_Buffers = null; }
+            if (m_AcqDevice != null)
+            { m_AcqDevice.Dispose(); m_AcqDevice = null; }
+        }
+        /// <summary>
+        /// 控制button状态
+        /// </summary>
+        void UpdateControls()
+        {
+            bool bAcqNoGrab = (m_Xfer != null) && (m_Xfer.Grabbing == false);
+            bool bAcqGrab = (m_Xfer != null) && (m_Xfer.Grabbing == true);
+            bool bNoGrab = (m_Xfer == null) || (m_Xfer.Grabbing == false);
+
+            // Acquisition Control
+            button_grab.Enabled = bAcqNoGrab;
+            //button_Snap.Enabled = bAcqNoGrab;
+            button_freeze.Enabled = bAcqGrab;
+
+            //// File Options
+            //button_New.Enabled = bNoGrab;
+            //button_Load.Enabled = bNoGrab;
+            //button_Save.Enabled = bNoGrab;
+
+            //button_Load_Config.Enabled = bAcqNoGrab;
+            //button_Buffer.Enabled = bNoGrab;
+        }
+        /// <summary>
+        /// 打开相机
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_opencamera_Click(object sender, EventArgs e)
+        {
+            AcqConfigDlg acConfigDlg = new AcqConfigDlg(null, "", AcqConfigDlg.ServerCategory.ServerAcqDevice);
+            if (acConfigDlg.ShowDialog() == DialogResult.OK)
+            {
+                if (!CreateNewObjects(acConfigDlg, false)) this.Close();
+            }
+            else
+            {
+                MessageBox.Show("No GigE-Vision cameras found or selected");
+                this.Close();
+            }
+        }
+        /// <summary>
+        /// 采集图像
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_grab_Click(object sender, EventArgs e)
+        {
+            this.StatusLabelInfo.Text = "";
+            //this.StatusLabelInfoTrash.Text = "";
+            if (m_Xfer.Grab())
+            {
+                UpdateControls();
+            }
+        }
+        /// <summary>
+        /// 停止采集
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_freeze_Click(object sender, EventArgs e)
+        {
+            AbortDlg abort = new AbortDlg(m_Xfer);
+            if (m_Xfer.Freeze())
+            {
+                if (abort.ShowDialog() != DialogResult.OK)
+                    m_Xfer.Abort();
+                UpdateControls();
+            }
+        }
+        /// <summary>
+        /// 关闭窗口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_closecamera_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        /// <summary>
+        /// 调整图像参数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_View_Click(object sender, EventArgs e)
+        {
+            ViewDlg viewDialog = new ViewDlg(m_View, m_ImageBox.ViewRectangle);
+
+            if (viewDialog.ShowDialog() == DialogResult.OK)
+                m_ImageBox.OnSize();
+
+            m_ImageBox.Refresh();
+        }
+    }
+}
